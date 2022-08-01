1a8aedf848a81d5451aef2445640cc8d885e548b
Amilcar Munoz <yellowballpython@gmail.com>
2022-08-01 00:50:58 -0600

    replace yargs with commander

--- index.ts
+++ index.ts
@@ -1,25 +1,34 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs'
 import colors from '@colors/colors/safe'
-import { hideBin } from 'yargs/helpers'
 import { findApiDir, findPagesDir, traverseDir } from './helpers'
+import { Command } from 'commander'
 
-yargs(hideBin(process.argv))
-  .scriptName('apext')
-  .command(
-    'list',
-    `🎯 ${colors.green('List all API endpoints in your Next.js project.')}`,
-    () => {},
-    async (argv) => {
+const program = new Command()
+
+program
+  .name('apext')
+  .description(
+    'A simple CLI tool to output all your Next.js API routes in your console.'
+  )
+  .version('0.0.2')
+
+program
+  .command('list')
+  .description(
+    `🎯 ${colors.green('List all API endpoints in your Next.js project.')}`
+  )
+  .action(async () => {
+    try {
       const pagesPath = await findPagesDir()
       const apiPath = await findApiDir(pagesPath)
 
       if (!apiPath) throw new Error('Could not find API directory')
 
       await traverseDir(apiPath)
+    } catch (error) {
+      console.log(error)
     }
-  )
-  .demandCommand(1)
-  .strict()
-  .parse()
+  })
+
+program.parse()

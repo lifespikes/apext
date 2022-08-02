528980900ca60f522c4b541cdf777aacd1f32a66
Amilcar Munoz <yellowballpython@gmail.com>
2022-08-02 00:08:12 -0600

    add arrow

--- lib/traverseDir.ts
+++ lib/traverseDir.ts
@@ -1,36 +1,42 @@
-import {readdir} from 'fs/promises'
-import colors from '@colors/colors/safe'
-import {isDirectory} from './isDirectory'
-import {getComments, getMethodParam} from "./utils/parser";
+import { readdir } from 'fs/promises'
+import colors, { white } from '@colors/colors/safe'
+import { isDirectory } from './isDirectory'
+import { getComments, getMethodParam } from './parser'
 
 const printEndpoint = async (parent: string, file: string) => {
-    const numberOfWhiteSpaces = 30;
-    const coloredPath = `${parent}/${colors.green(file)}`
-    const comments = await getComments(`${parent}/${file}`);
-    const {methods, originalLength} = getMethodParam(comments);
+  const numberOfWhiteSpaces = 44
+  const coloredPath = `${parent}/${colors.green(file)}`
+  const comments = await getComments(`${parent}/${file}`)
+  const { methods } = getMethodParam(comments)
 
-    const value = numberOfWhiteSpaces - originalLength;
+  const value = numberOfWhiteSpaces - coloredPath.length
 
-    const whiteSpaces = Array(value < 0 ? 0 : value).fill(' ').join("");
+  const whiteSpaces = Array(value < 0 ? 0 : value).fill('-')
+  whiteSpaces.push('>')
+  const arrow = whiteSpaces.join('')
 
-    console.log(`${methods}${whiteSpaces} ${coloredPath}`)
+  if (methods.length > 0) {
+    console.log(`${coloredPath} ${arrow} ${methods}`)
+  } else {
+    console.log(`${coloredPath}`)
+  }
 }
 
 export const traverseDir = async (path: string, parent: string = '/api') => {
-    try {
-        const dirs = await readdir(path)
-        for (const file of dirs) {
-            const absolutePath = `${path}/${file}`
-            const relativePath = `${parent}/${file}`
-            if (isDirectory(absolutePath)) {
-                await traverseDir(absolutePath, relativePath)
-            } else {
-                await printEndpoint(parent, file)
-            }
-        }
-    } catch (error: any) {
-        if (typeof error === 'string') throw colors.red(error)
-        else throw error
+  try {
+    const dirs = await readdir(path)
+    for (const file of dirs) {
+      const absolutePath = `${path}/${file}`
+      const relativePath = `${parent}/${file}`
+      if (isDirectory(absolutePath)) {
+        await traverseDir(absolutePath, relativePath)
+      } else {
+        await printEndpoint(parent, file)
+      }
     }
-    return
+  } catch (error: any) {
+    if (typeof error === 'string') throw colors.red(error)
+    else throw error
+  }
+  return
 }

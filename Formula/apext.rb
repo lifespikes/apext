193a4b6758701a5b9c48e29042707b508e2d25e5
Amilcar Munoz <yellowballpython@gmail.com>
2022-10-21 17:53:40 -0600

    update Formula

--- Formula/apext.rb
+++ Formula/apext.rb
@@ -1,9 +1,9 @@
 class Apext < Formula
     desc "A simple CLI tool to manage and organize your Next.js API routes."
     homepage "https://github.com/lifespikes/apext"
-    url "https://github.com/lifespikes/apext/releases/download/v0.2.2/apext-macos-x64.tar.gz"
-    sha256 "18573a80b8093035376f338ff789bf5c87807177cf5fbdb0bbe62ca7b8592d9e"
-    version "0.2.2"
+    url "https://github.com/lifespikes/apext/releases/download/v0.3/apext-macos-x64.tar.gz"
+    sha256 "fc647d2c2c9f7f0dff3126ec3bc479e19f2b209e409598bf9b2c9eb846831db9"
+    version "0.3"
     def install
       bin.install "apext"
     end
